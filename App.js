2999f873d5916e6d31dfb181f8a694b7253cdc0a
Guilherme <Guilherme>
2024-07-23 11:51:54 -0300

    introduction react native

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, } from 'react-native';
+import React from 'react';
+import Logo from './assets/Pages/vascu.jpg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styless.cont}>quem ta lendo eh </Text>
+      <Image style={styless.img} source={Logo}></Image>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    background: 'linear-gradient(45deg, rgba(19,170,255,1) 0%, rgba(63,0,198,1) 3F00C6%)',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#444',
+    color: '#fffff',
   },
+
+  }
+);
+
+const styless = StyleSheet.create({
+  cont: {
+    fontSize: '40px',
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  img: {
+    width: 400,
+    height: 400,
+  }
 });
